175867ab2af4f81e14e682e1302967761ac9f46a
Castro <ceafive@gmail.com>
2019-08-19 10:13:16 +0000

    add landing and portfolio page

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+
+//        devblack: '#00030B',
+//        devblue: '#0043FB',
+//        devnavy: '#002DA5',
+//        devdarkblue: '#011B5B',
+//        devgray: '#4A5765',
+//        devash: '#C7CED3',
+//        devpaleblue: '#406693'
+
+module.exports = {
+  theme: {
+    extend: {
+        colors: {
+        devblack: '#00030B',
+        devblue: '#028DFF',
+        devblue2: '#00BEFF',
+        devnavy: '#003572',
+        devdarkblue: '#001030',
+        devdarkblue2: '#000817',
+        devgray: '#4A5765',
+        devash: '#F9F8FE',
+        devpaleblue: '#207ECF',
+        devred: '#ff1d5e',
+        twitter: '#38A1F3',
+        linkedin: '#0077B5',
+        whatsapp: '#25d366'
+      },
+        borderRadius: {
+            br1: '2rem',
+            br2: '4rem'
+        },
+        fontSize: {
+            '10xl': '10rem'
+        }
+    }
+  },
+  variants: {},
+  plugins: []
+}
